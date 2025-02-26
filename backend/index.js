8d0c6d9c9fa913a93582b3bd45ad54c8d988c712
Aayush <aayush.dl8901@gmail.com>
2025-02-26 20:37:58 +0530

    update project structure

--- backend/index.js
+++ backend/index.js
@@ -45,18 +45,22 @@ app.use("/api/v1/chats", chat);
 app.use("/api/v1/messages", message);
 
 
-const __dirname1 = path.resolve()
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static(path.join(__dirname1, '/frontend/build')))
+app.get('/', (req, res)=>{
+  app.send('Server is up and running!')
+})
 
-  app.get('*', (req,res)=>{
-    res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
-  })
-}else{
-  app.get('/', (req, res)=>{
-    res.send('Api is running successfully')
-  })
-}
+// const __dirname1 = path.resolve()
+// if(process.env.NODE_ENV==='production'){
+//   app.use(express.static(path.join(__dirname1, '/frontend/build')))
+
+//   app.get('*', (req,res)=>{
+//     res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"))
+//   })
+// }else{
+//   app.get('/', (req, res)=>{
+//     res.send('Api is running successfully')
+//   })
+// }
 
 //error middlewares
 
